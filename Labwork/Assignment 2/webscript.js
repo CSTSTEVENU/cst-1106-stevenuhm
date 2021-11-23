0547a401529875b0e7ff731bd40b3e11ceed8a68
CSTSTEVENU <91871545+CSTSTEVENU@users.noreply.github.com>
2021-11-23 00:29:20 -0800

    Create webscript.js

---
+++ Labwork/Assignment 2/webscript.js
@@ -0,0 +1,84 @@
+function addition() {
+  x = parseInt(jQuery('value1').val());
+  y = parseInt(jQuery('value2').val());
+  z = x + y
+  jQuery('#Result').html(z);
+  tmp = '<span id="">' + x + ' + ' + y ' = ' + z + ' <button id="delete"> Delete </button> <br> </span>';
+  jQuery('#History').append(tmp);
+
+}
+
+function subtraction() {
+  x = parseInt(jQuery('value1').val());
+  y = parseInt(jQuery('value2').val());
+  z = x - y
+  jQuery('#Result').html(z);
+  tmp = ('<span id="">' + x + ' - ' + y + ' = ' + z + )' <button id="delete"> Delete </button> <br> </span>';
+  jQuery('#History').append(tmp);
+
+}
+
+function multiplcation() {
+  x = parseInt(jQuery('value1').val());
+  y = parseInt(jQuery('value2').val());
+  z = x * y
+  jQuery('#Result').html(z);
+  tmp = ('<span id="">' + x + ' * ' + y + ' = ' + z + )' <button id="delete"> Delete </button> <br> </span>';
+  jQuery('#History').append(tmp);
+
+}
+
+function division() {
+  x = parseInt(jQuery('value1').val());
+  y = parseInt(jQuery('value2').val());
+  z = x / y
+  jQuery('#Result').html(z);
+  tmp = ('<span id="">' + x + ' / ' + y + ' = ' + z + )' <button id="delete"> Delete </button> <br> </span>';
+  jQuery('#History').append(tmp);
+
+}
+
+function power() {
+  x = parseInt(jQuery('value1').val());
+  y = parseInt(jQuery('value2').val());
+
+  z = 1;
+
+  for(let i=1; i<=y; i+=1) {
+    z *= x
+  }
+
+  jQuery('#Result').html(z);
+  tmp = ('<span id="">' + x + ' ^ ' + y + ' = ' + z + )' <button id="delete"> Delete </button> <br> </span>';
+  jQuery('#History').append(tmp);
+
+}
+
+function show() {
+  jQuery('#History').attr('style', '');
+
+}
+
+function hide() {
+  jQuery('#History').attr('style', 'display:none');
+
+}
+
+function deleteElement() {
+  jQuery(this).parent().hide();
+
+}
+
+function setup() {
+  jQuery('#trigger_addition').click(addition);
+  jQuery('#trigger_subtract').click(subtraction);
+  jQuery('#trigger_muliplication').click(multiplcation);
+  jQuery('#trigger_division').click(division);
+  jQuery('#trigger_power').click(power);
+  jQuery('#show').click(show);
+  jQuery('#hide').click(hide);
+  jQuery('body').on('click', '#delete', deleteElement);
+
+}
+
+jQuery(document).ready(setup);
